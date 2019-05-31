46bfde35280ad24deccbcacce6eefbddd3cccdfc
kangogopeter <peterkangogo501@gmail.com>
2019-05-31 17:37:56 +0300

    adding a file in the script.js

--- src/main/resources/public/js/script.js
+++ src/main/resources/public/js/script.js
@@ -11,3 +11,53 @@ $(document).ready(function () {
   }
 });
 });
+
+
+var myVideo = {"video": true},errBack = function(error){
+alert("Why hello Newton");
+};
+if(navigator.getUserMedia){
+navigator.getUserMedia(myVideo,startWebcam, errBack);
+}else if(navigator.webkitGetUserMedia){
+navigator.webkitGetUserMedia(myVideo,startWebcam,errBack);
+}else if(navigator.mozGetUserMedia){
+navigator.mozGetUserMedia(myVideo,startWebcam,errBack);
+};
+
+function startWebcam(stream){
+var onlinePeter = getElementById("onlinePeter"),
+    video = onlinePeter.querySelectorAll("video"),
+    canvas = onlinePeter.querySelectorAll("canvas"),
+video.width = video.offsetWidth;
+if(navigator.getUserMedia){
+video.src = stream;
+video.play();
+}else if(navigator.webkitGetUserMedia){
+video.src = window.webkitURL.createObjectUrl(stream);
+video.play();
+}else if(navigator.mozGetUserMedia){
+video.src = window.URL.createObjectUrl(stream);
+video.play();
+};
+
+$("#webcam-popup .takephoto").click(function(){
+var take = document.createElement("canvas");
+take.width = video.offsetWidth;
+take.height = video.offsetHeight;
+
+var takecontext = take.getcontext("2d"),
+takeScale = (take.height/take.width);
+take.drawImage(
+video,
+0,0,
+video.offsetWidth,video.offsetHeight
+);
+canvas.style.height = parseInt(canvas.offsetWidth * takeScale);
+canvas.width = canvas.offsetWidth;
+canvas.height = canvas.offsetHeight;
+var context = canvas.getContext("2d"),
+scale = canvas.width/take.width;
+context.scale(scale,scale);
+context.drawImage(bigimage, 0, 0);
+});
+};

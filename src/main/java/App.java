70bb6dc28b594a297e249eb6f92a9a624a1144f5
kangogopeter <peterkangogo501@gmail.com>
2019-05-31 19:38:35 +0300

    adding a file in the pom.xml file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,6 +5,7 @@ import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
+    static int getHer
     public static void main(String[] args) {
         staticFileLocation("/public");
         String layout = "templates/layout.vtl";

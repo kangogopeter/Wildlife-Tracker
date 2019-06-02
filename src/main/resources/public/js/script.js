a21bac60d2ab86a12a9a17e8ff3e1f5084bb5ef1
kangogopeter <peterkangogo501@gmail.com>
2019-06-02 18:27:24 +0300

    creating a photo function

--- src/main/resources/public/js/script.js
+++ src/main/resources/public/js/script.js
@@ -11,3 +11,65 @@ $(document).ready(function () {
   }
 });
 });
+
+
+var videoObj    = { "video": true },
+    errBack        = function(error){
+        // alert("Video capture error: ", error.code);
+    };
+
+
+if(navigator.getUserMedia){                    // Standard
+    navigator.getUserMedia(videoObj, startWebcam, errBack);
+}else if(navigator.webkitGetUserMedia){        // WebKit
+    navigator.webkitGetUserMedia(videoObj, startWebcam, errBack);
+}else if(navigator.mozGetUserMedia){        // Firefox
+    navigator.mozGetUserMedia(videoObj, startWebcam, errBack);
+};
+
+function startWebcam(stream){
+
+    var myOnlineCamera    = getElementById('myOnlineCamera'),
+        video            = myOnlineCamera.querySelectorAll('video'),
+        canvas            = myOnlineCamera.querySelectorAll('canvas');
+
+    video.width = video.offsetWidth;
+
+    if(navigator.getUserMedia){                    // Standard
+        video.src = stream;
+        video.play();
+    }else if(navigator.webkitGetUserMedia){        // WebKit
+        video.src = window.webkitURL.createObjectURL(stream);
+        video.play();
+    }else if(navigator.mozGetUserMedia){
+        video.src = window.URL.createObjectURL(stream);
+        video.play();
+    };
+
+
+    $('#webcam-popup .takephoto').click(function(){
+
+        var temp = document.createElement('canvas');
+
+        temp.width  = video.offsetWidth;
+        temp.height = video.offsetHeight;
+
+        var tempcontext = temp.getContext("2d"),
+            tempScale = (temp.height/temp.width);
+
+        temp.drawImage(
+            video,
+            0, 0,
+            video.offsetWidth, video.offsetHeight
+        );
+
+
+        canvas.style.height    = parseInt( canvas.offsetWidth * tempScale );
+        canvas.width        = canvas.offsetWidth;
+        canvas.height        = canvas.offsetHeight;
+        var context        = canvas.getContext("2d"),
+            scale        = canvas.width/temp.width;
+        context.scale(scale, scale);
+        context.drawImage(bigimage, 0, 0);
+    });
+};
